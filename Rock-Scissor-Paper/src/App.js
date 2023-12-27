708b71c6a4dc0c92adb0ae7aeb932b2c331ef1c8
Jun Seong <drawbeat0112@gmail.com>
2023-12-27 15:11:37 +0900

    게임 스코어에 따른 결과알림창 업데이트

--- Rock-Scissor-Paper/src/App.js
+++ Rock-Scissor-Paper/src/App.js
@@ -32,20 +32,35 @@ function App() {
   const handleButtonClick = (nextHand) => {
     // utils.js에 있는 generateRandomHand 함수 상수에 저장
     const nextOtherHand = generateRandomHand();
+
     // 승리 또는 무승부 또는 패배를 전달 받기위한 함수를 상수에 저장
     const nextHistoryItem = getResult(nextHand, nextOtherHand)
+
     // utils.js에 있는 compaerHand 함수는 양쪽의 값을 비교했을 때 0 , 1 , -1이 나오는 값을 상수에 저장
     const comparison = compareHand(nextHand, nextOtherHand)
+
     // 아규먼트로 paper를 전달 setHand는 paper로 상태변경
     setHand(nextHand);
+    
     // setOtherHand  실행 값은 scissor로 상태변경
     setOtherHand(nextOtherHand);
+
     // 배열안에 결과값들을 새로운 배열로 반환하기 위해서는 스프레드 구문을이용해서 반환해야함
     setGameHistory([...gameHistroy, nextHistoryItem])
+
     // compareHand에서 1이 나올 경우 기존 스코어 숫자 더하기 input에 있는 value 값을 더함
     if (comparison > 0) setScore (score + bet);
+
     // compareHand에서 -1이 나올 경우 상대방 스코어 수 더하기 input에 있는 value 값을 더함
     if (comparison < 0) setOtherScore(otherScore + bet);
+
+    if(score >= 50){
+      alert('축하합니다! 게임에서 이겼습니다.')
+      handleClearClick();
+    }else if(otherScore >= 50){
+      alert('아쉽지만! 게임에서 지셨어요.')
+      handleClearClick();
+    }
   };
   
   const handleClearClick = () => {
@@ -68,43 +83,45 @@ function App() {
   }
   
   return (
-    <div id='App'> 
-      {/* handleClearClick 함수를 실행 */}
-      <header className='App-header'>
-        <h1 className='App-heading'>가위 바위 보</h1>
-        <Button className='App-reset' onClick={handleClearClick}></Button> 
-      </header>
-      <Score score={score} otherScore={otherScore} />
-      <section className='App-borads'>
-        <div className='Box-inner'>
-          <div className='Borads-inner'>
-            {/* paper */}
-            <div className='Hand'>
-              <HandIcon value={hand} />
+    <div id='App'>
+      <main id='Main'>
+        {/* handleClearClick 함수를 실행 */}
+        <header className='App-header'>
+          <h2 className='App-heading'>가위 바위 보</h2>
+          <Button className='App-reset' onClick={handleClearClick}></Button> 
+        </header>
+        <Score score={score} otherScore={otherScore} />
+        <section className='App-borads'>
+          <div className='Box-inner'>
+            <div className='Borads-inner'>
+              {/* paper */}
+              <div className='Hand'>
+                <HandIcon value={hand} />
+              </div>
+              <p className='Borads-vs'>VS</p>
+              {/* scissor */}
+              <div className='Hand'>
+                <HandIcon value={otherHand} />
+              </div>
             </div>
-            <p className='Borads-vs'>VS</p>
-            {/* scissor */}
-            <div className='Hand'>
-              <HandIcon value={otherHand} />
+            <div className='Borads-inputs'>
+              <span>배점</span>
+              <input type='number' value={bet} step={1} min={1} max={9} onChange={handleBetChange} />
+              <span>배</span>
+            </div>
+            <div className='Borads-result'>
+              <h3>승부 기록</h3>
+              <p>{gameHistroy.join(', ')}</p>
             </div>
           </div>
-          <div className='Borads-inputs'>
-            <span>배점</span>
-            <input type='number' value={bet} step={1} min={1} max={9} onChange={handleBetChange} />
-            <span>배</span>
-          </div>
-          <div className='Borads-result'>
-            <h3>승부 기록</h3>
-            <p>{gameHistroy.join(', ')}</p>
-          </div>
+        </section>
+        <div>
+          <HandButton value='rock' onClick = {handleButtonClick} />
+          <HandButton value='scissor' onClick = {handleButtonClick} />
+          {/* paper를 눌렀을 때 */}
+          <HandButton value='paper' onClick = {handleButtonClick} /> 
         </div>
-      </section>
-      <div>
-        <HandButton value='rock' onClick = {handleButtonClick} />
-        <HandButton value='scissor' onClick = {handleButtonClick} />
-        {/* paper를 눌렀을 때 */}
-        <HandButton value='paper' onClick = {handleButtonClick} /> 
-      </div>
+      </main>
     </div>
   );
 }
