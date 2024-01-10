278821a7e28d8e37e17a3fb7314578ceb06e268c
Jun Seong <drawbeat0112@gmail.com>
2024-01-10 23:34:45 +0900

    dice design css fix

--- Dice-Game/src/Board.js
+++ Dice-Game/src/Board.js
@@ -1,5 +1,5 @@
 import Dice from "./Dice";
-import './styles/Board.css'
+import "./styles/Board.css";
 
 function Board({ name, color, gameHistory }) {
   const num = gameHistory[gameHistory.length - 1] || 1;
@@ -7,12 +7,18 @@ function Board({ name, color, gameHistory }) {
 
   return (
     <section>
-      <h2>{name}</h2>
-      <Dice color={color} num={num} />
-      <h2>총점</h2>
-      <p>{sum}</p>
-      <h2>기록</h2>
-      <p>{gameHistory.join(", ")}</p>
+      <article className="header-sec">
+        <h2>{name}</h2>
+        <Dice color={color} num={num} />
+      </article>
+      <article className="score-sec">
+        <h2>총점</h2>
+        <p>{sum}</p>
+      </article>
+      <article className="history-sec">
+        <h2>기록</h2>
+        <p>{gameHistory.join(", ")}</p>
+      </article>
     </section>
   );
 }
