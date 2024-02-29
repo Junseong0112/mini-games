fcb41824f251fbd77ce901a0551bc75b6fe86937
Jun Seong <drawbeat0112@gmail.com>
2024-03-01 00:11:16 +0900

    tic-tac-toe file remove

--- tic-tac-toe/src/components/Backdrop.js
+++
@@ -1,7 +0,0 @@
-function Backdrop () {
-  return (
-    <div id="backdrop"></div>
-  )
-}
-
-export default Backdrop;
--- tic-tac-toe/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-function Footer () {
-  return (
-    <footer>
-      <h2>Make By Junseong</h2>
-    </footer>
-  )
-}
-
-export default Footer;
