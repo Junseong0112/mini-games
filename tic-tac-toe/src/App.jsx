d60c2367c6cdffc48e3734852468517b8bf7b65f
Jun Seong <drawbeat0112@gmail.com>
2024-03-01 23:13:16 +0900

    GameBoard 컴포넌트 간소화 및 App 컴포넌트에 기존 보드판 이동

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -2,6 +2,13 @@ import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 import { useState } from "react";
 import Log from "./components/Log";
+import { WINNING_COMBINATIONS } from "./winning-combinations.js";
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
 
 function deriveActivePlayer(gameTurns) {
   let currentPlayer = "X";
@@ -16,7 +23,25 @@ function deriveActivePlayer(gameTurns) {
 function App() {
   // gameTurns state를 이용하여 GameBoard, Log 컴포넌트에 상태를 업데이트 할 수 있기에 가독성 및 효율적으로 코드를 작성이 가능하다.
   const [gameTurns, setGameTurns] = useState([]);
+
   const activePlayer = deriveActivePlayer(gameTurns);
+
+  let gameBoard = initialGameBoard;
+
+  for (const turn of gameTurns) {
+    // console.log(turn);
+    const { square, player } = turn;
+    const { row, col } = square;
+
+    gameBoard[row][col] = player;
+  }
+
+  for (const combination of WINNING_COMBINATIONS) {
+    console.log(combination);
+    // const firstSquareSymbol = gameBoard[]
+    // const secondSquareSymbol
+    // const thirdSquareSymbol
+  }
 
   const handleSelectSquare = (rowIndex, colIndex) => {
     setGameTurns((prevTurns) => {
@@ -46,7 +71,7 @@ function App() {
             isActive={activePlayer === "O"}
           />
         </ol>
-        <GameBoard onSlectSquare={handleSelectSquare} turns={gameTurns} />
+        <GameBoard onSlectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
     </main>
--- tic-tac-toe/src/components/GameBoard.jsx
+++ tic-tac-toe/src/components/GameBoard.jsx
@@ -1,42 +1,7 @@
-const initialGameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
-
-export default function GameBoard({
-  onSlectSquare,
-  activePlayerSymbol,
-  turns,
-}) {
-  let gameBoard = initialGameBoard;
-
-  for (const turn of turns) {
-    console.log(turn);
-    const { square, player } = turn;
-    const { row, col } = square;
-
-    gameBoard[row][col] = player;
-  }
-  // App 컴포넌트로 상태를 끌어올려서 사용했기 때문에 아래 코드는 주석처리, log 컴포넌트와 같이 사용하기 위해서 끌어올려서 사용
-  // const [gameBoard, setGameBoard] = useState(initialGameBoard);
-
-  // const handleSelectSquare = (rowIndex, colIndex) => {
-  //   setGameBoard((prevGameBoard) => {
-  //     // 객체, 배열은 원본을 훼손시키지 않고 복사하여 새로운 객체, 배열로 반환해야한다.
-  //     const updatedBoard = [
-  //       ...prevGameBoard.map((innerArray) => [...innerArray]),
-  //     ];
-  //     updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-  //     return updatedBoard;
-  //   });
-  //   // player가 변경되게 하는 함수
-  //   onSlectSquare();
-  // };
-
+export default function GameBoard({ onSlectSquare, board }) {
   return (
     <ol id="game-board">
-      {gameBoard.map((row, rowidx) => (
+      {board.map((row, rowidx) => (
         <li key={rowidx}>
           <ol>
             {row.map((playerSymbol, colIdx) => (
