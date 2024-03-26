1f9269c640798a094843a3cf7e774d21041df766
Jun Seong <drawbeat0112@gmail.com>
2024-03-26 18:18:28 +0900

    Header 컴포넌트 추가

---
+++ tic-tac-toe/src/components/Header.tsx
@@ -0,0 +1,12 @@
+import GameLogo from '../assets/game-logo.png'
+
+const Header = () => {
+  return(
+    <header>
+      <img src={GameLogo} alt='game-logo' />
+      <h1>Tic-Tac-Toe</h1>
+    </header>
+  )
+}
+
+export default Header
